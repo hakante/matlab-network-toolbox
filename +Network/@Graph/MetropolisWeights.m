18a3374192ee9ba4a4b504b37b587544881ccfd0
Håkan Terelius <hakante@kth.se>
2013-02-06 14:56:52 +0100

    Sparse implementation of Metropolis weights

--- +Network/@Graph/MetropolisWeights.m
+++ +Network/@Graph/MetropolisWeights.m
@@ -1,12 +1,21 @@
 function metropolisWeights = MetropolisWeights( g )
 
+    diagonalIndices = 1 : g.numberOfNodes+1 : g.numberOfNodes^2;
+
     adjacencyMatrixWithoutSelfloops = g.adjacencyMatrix;
-    adjacencyMatrixWithoutSelfloops(1:g.numberOfNodes+1:g.numberOfNodes^2) = 0;
+    adjacencyMatrixWithoutSelfloops(diagonalIndices) = 0;
+    
+    maxRow = sum(adjacencyMatrixWithoutSelfloops,2);
+    maxCol = sum(adjacencyMatrixWithoutSelfloops,1);
 
-    maxInOutDegrees = max(sum(adjacencyMatrixWithoutSelfloops,2)*ones(1,g.numberOfNodes), ...
-        ones(g.numberOfNodes,1)*sum(adjacencyMatrixWithoutSelfloops,1));
-
-    metropolisWeights = adjacencyMatrixWithoutSelfloops./((maxInOutDegrees+ones(g.numberOfNodes)));
-    metropolisWeights = diag(1-sum(metropolisWeights,1))+metropolisWeights;
+    if issparse(adjacencyMatrixWithoutSelfloops)
+        [nzr, nzc, nzv] = find(adjacencyMatrixWithoutSelfloops);
+        metropolisWeights = sparse(nzr, nzc, nzv ./ (max(maxRow(nzr),maxCol(nzc)')+1), g.numberOfNodes, g.numberOfNodes);
+    else
+        maxInOutDegrees = max(maxRow*ones(1,g.numberOfNodes), ones(g.numberOfNodes,1)*maxCol);
+        metropolisWeights = adjacencyMatrixWithoutSelfloops ./ (maxInOutDegrees+1);
+    end
+    
+    metropolisWeights(diagonalIndices) = 1 - sum(metropolisWeights,1);
     
 end
